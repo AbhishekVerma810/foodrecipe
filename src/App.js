c54502cccfc96f0d78c29b0002a58481463a9130
Abhishek <abhishekverma8103@gmail.com>
2023-08-02 17:21:09 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './componets/login';
+import Home from './componets/home';
+import Signup from './componets/signup';
+import Recipe from './componets/Recipe';
+import Complaterecipe from './componets/complaterecipe'
+import { AuthProvider, useAuth } from './context/AuthContext';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  
+  const { currentUser } = useAuth();
 
-export default App;
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to='/' />;
+    }
+    return children;
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={ // Move the ProtectedRoute inside the element prop
+            <ProtectedRoute>
+              <Home/>
+            </ProtectedRoute>
+            }
+          />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path='/recipe' element={<Recipe />} />
+        <Route path='/Complaterecipe/:recipeId' element={<Complaterecipe/>}/>
+      </Routes>
+    </Router>
+  );
+};
+const WrappedApp = () => {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+};
+export default WrappedApp;
